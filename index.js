7b5ff53dc752409096f90c31e8bcdaac60ce123f
jsmojnu <learnwithmojnu@gmail.com>
2022-01-02 15:26:35 +0600

    update-server

--- index.js
+++ index.js
@@ -1,54 +1,44 @@
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
-var http = require('https');
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-require('dotenv').config();
-const port = 5000;
-
-
+const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jvd1e.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+require('dotenv').config()
 
+const app = express()
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 5000;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jvd1e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const playerCollection = client.db("players").collection("cricket");
-
-  console.log('connected');
+  const collection = client.db("players").collection("cricket");
   // perform actions on the collection object
-  app.post('/addPlayer', (req, res) => {
-      const player = req.body;
-      playerCollection.insertOne(player)
+  app.post('/api/addPlayer', (req, res) => {
+      const data = req.body;
+      collection.insertOne(data)
       .then(result => {
           res.send(result.insertCount > 0);
       })
   })
+  app.get('/api/players', (req, res) => {
+    collection.find({}).sort({ highScore: -1 })
+      .toArray((err, documents) => {
+          res.send(documents);
+      })
+  })
 
-  app.get('/players', (req, res) => {
-    playerCollection.find({}).sort({ highScore: -1 })
-   .toArray((err, documents) => {
-     res.send(documents);
-   })
- })
-
- // filter Specific name
- app.get('/:nickname', (req, res) => {
-   playerCollection.find({}).filter({ nickname: req.params.nickname })
-   .toArray((err, documents) => {
-     res.send(documents);
-   })
- })
- client.close();
+  // filter Specific name
+ app.get('/api/:nickname', (req, res) => {
+    collection.find({}).filter({ nickname: req.params.nickname })
+    .toArray((err, documents) => {
+      res.send(documents);
+    })
+  })
 });
 
-app.get('/', (req, res) => {
-  res.send('I am working...');
+app.get('/', (req, res) =>{
+    res.send('working...');
 })
 
 app.listen(process.env.PORT || port);
